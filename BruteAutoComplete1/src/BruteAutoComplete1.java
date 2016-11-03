e302f2875af460aba60fc60acb0e24ecbda41ffb
sean kearney <s.kearney.94@gmail.com>
2016-11-03 03:09:06 +0000

    Commit this to memory

---
+++ BruteAutoComplete1/src/BruteAutoComplete1.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+public class BruteAutoComplete1 implements AutoComplete{
+	private ArrayList<Term> words;
+	private Scanner input;
+	private Stopwatch stopwatch;
+	
+	public static void main(String[] args) throws Exception
+	  {
+		//calls the main method
+		  new BruteAutoComplete1();
+	  }
+	
+	public BruteAutoComplete1() throws FileNotFoundException
+	{
+		stopwatch = new Stopwatch();
+		//loads the userfile from the location on my computer and adds them to an arraylist using scanner
+		input = new Scanner(System.in);
+		File usersFile = new File("C:/Users/seankearney/Documents/Workspace/BruteAutoComplete1/data/userdata.txt");
+		  Scanner inWords = new Scanner(usersFile);
+		  words= new ArrayList<Term>();
+		  String delims = "[	]"; //Seperates feilds by tab.
+		  while (inWords.hasNextLine()) {
+		    // Gets the word from the arraylist
+		    String word = inWords.nextLine();
+		    //gets the other half of the fields that where seperated
+		    String[] termArray = word.split(delims);
+		    //assigns the weight and words to a variable
+		    if (termArray.length > 1) {
+		      double weight = Double.parseDouble(termArray[0]);
+		      words.add(new Term(weight,termArray[1]));
+		    }
+		  }
+		  
+		  Collections.sort(words, new Comparator<Term>(){
+			@Override
+			public int compare(Term t1, Term t2) {
+				return (int) (t2.getWeight()-t1.getWeight());
+			}});
+		  System.out.println("Loading in takes : "+ stopwatch.elapsedTime());
+		  //calls the main menu function
+		  
+	}
+	
+	
+
+	public double weightOf(String term) {
+		//checks arraylist if anything starts with term and returns the weight
+		for(Term storeWord:words)
+			if(storeWord.getWord().equals(term))
+				return storeWord.getWeight();
+		return 0;
+	}
+
+	public String bestMatch(String prefix) {
+		//checks arraylist if anything starts with term and adds it to the result string.
+		for(Term word : words)
+		{
+			if(word.getWord().startsWith(prefix))
+				return word.getWord();
+		}
+		return null;
+	}
+
+	@Override
+	public Iterable<String> matches(String prefix, int k) {
+		List<String> result = new ArrayList<String>();
+		
+		for(Term storeWord:words)
+			if(storeWord.getWord().startsWith(prefix) && k > 0)
+			{
+				result.add(storeWord.getWord());
+				k--;
+			}
+		return result;
+	}
+}
---
+++ BruteAutoComplete1/src/Term.java
@@ -0,0 +1,32 @@
+
+public class Term {
+double weight;
+String word;
+public Term(double weight, String word){
+	setWeight(weight);
+	setWord(word);
+}
+@Override
+public String toString() {
+	return "StoreWord [weight=" + weight + ", word=" + word + "]";
+}
+public double getWeight() {
+	return weight;
+}
+public void setWeight(double weight) {
+	if(weight > 0)
+		this.weight = weight;
+	else
+		throw new IllegalArgumentException();
+}
+public String getWord() {
+	return word;
+}
+public void setWord(String word) {
+	if(word != null)
+		this.word = word;
+	else
+		throw new IllegalArgumentException();
+}
+
+}
